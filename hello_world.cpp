1b2d7a0b950def7ba2e05b80940defa2d6365aa0
Ekaterina <karpina.katia@gmail.com>
2025-03-16 14:52:53 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     std::string name;
-    std::cout << "Введите ваше имя: ";//your name
+    std::cout << "Введите ваше имя: ";//your name!
     std::cin >> name;
     std::cout << "Hello world from " << name << "!" << std::endl;
     return 0;
